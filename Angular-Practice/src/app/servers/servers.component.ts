f6150b6cabd85b946e323a96cab1318514a598f6
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-29 01:28:38 +0500

    implemented event binding by adding click event.

--- Angular-Practice/src/app/servers/servers.component.ts
+++ Angular-Practice/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreatedStatus = 'No Server was created!';
 
   constructor() {
     setTimeout(() => {
@@ -17,5 +18,7 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onCreateServer(){
+    return this.serverCreatedStatus = 'A New Server Created'!
+  }
 }
