f11a8bd37b36b7ad5f699366da3e95e0c106847e
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-05-29 06:53:29 +0500

    Implemented Ingrediants model on Shopping List to validate the structure

--- Angular-Practice/src/app/shared/models/ingrediants.ts
+++ Angular-Practice/src/app/shared/models/ingrediants.ts
@@ -1,5 +1,5 @@
-export class ingrediants{
+export class Ingrediants{
 
-    constructor(public name : string, public amount : string){
+    constructor(public name : string, public amount : number){
     }
 }
